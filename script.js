4af77cff32e51fc6aabf9979a373ee3ebfe15abb
AshwathSanjai <136235110+AshwathSanjai@users.noreply.github.com>
2024-03-01 23:20:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+VANTA.BIRDS({
+    el: "#background",
+    mouseControls: true,   
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xbbf0,
+    color1: 0x2e6b72,
+    birdSize: 2.00,
+    speedLimit: 7.00,
+    separation: 49.00 
+  })
+
+  
